a8a1a81efd3f50b4b7682ffecb41a1f70f9d76b5
Rabia-Ghafoor14 <rabiaghafoor482@gmail.com>
2026-05-25 01:16:46 +0500

    chore: ignore local nextjs build artifacts

--- .next/dev/server/chunks/ssr/pages__app_jsx_034wwud._.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-"[project]/pages/_app.jsx [ssr] (ecmascript)", ((__turbopack_context__) => {
-"use strict";
-
-__turbopack_context__.s([]);
-}),
-];
-
-//# sourceMappingURL=pages__app_jsx_034wwud._.js.map
